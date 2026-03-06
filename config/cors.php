b4ba44a78f404566fb61988230265d5d1d9f40f8
Bernaldo Napitupulu <bernaldonapitupulu@Bernaldos-MacBook-Pro.local>
2026-03-07 01:27:13 +0800

    perbaikan cors

---
+++ config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://kuy.next-level-study.com',
+        'http://localhost:5173',
+        'https://osn.next-level-study.com',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
